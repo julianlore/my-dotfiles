b857f1dca48d2e5cb247700408c336d4b7ac91c3
Julian Lore <julian.lore@mail.mcgill.ca>
2019-05-23 21:56:41 -0400

    Update configure script to copy over files from misc

--- configure.sh
+++ configure.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
-git submodule update --init --recursive
-if [ -d /usr/share/zsh ] && [ -d /usr/share/oh-my-zsh ]
-then
-    sudo ln -s /usr/share/zsh/plugins/* /usr/share/oh-my-zsh/plugins
-fi
-git config --global core.excludesfile '~/.gitignore'
+# git submodule update --init --recursive
+# if [ -d /usr/share/zsh ] && [ -d /usr/share/oh-my-zsh ]
+# then
+#     sudo ln -s /usr/share/zsh/plugins/* /usr/share/oh-my-zsh/plugins
+# fi
+# git config --global core.excludesfile '~/.gitignore'
+
+copymisc () {
+    dirname $1 | sed -e "s/Misc\//Misc\/Backups\//" | xargs mkdir -p
+    realpath=$(echo $1 | sed -e "s/Misc//")
+    if [ -f $realpath ]
+    then
+        backup=$(echo $1 | sed -e "s/Misc\//Misc\/Backups\//")
+        if [ ! -f $backup ]
+        then
+            cp $realpath
+        fi
+    fi
+    sudo cp $1 $realpath
+}
+export -f copymisc
+mkdir -p Misc/Backups
+echo "Backing up existing files to Misc/Backups"
+find Misc -mindepth 2 -type f ! -name "Backups*" -exec bash -c 'copymisc "$0"' {} \;
+sudo grub-mkconfig -o /boot/grub/grub.cfg
